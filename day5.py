fde514ac1c98ee13070e6d5656de844fbf04a430
Pujitha <pujithamayakunta16@gmail.com>
2026-06-03 20:15:30 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,24 @@
+#string allocation
+name = "Pujitha"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[6])
+#String Slicing
+s  = "Hello , World!"
+print(s[1:-1:2])
+
+#string length
+string = "Hello , World!"
+print(len(string))
+#String Concatenation
+first_name = "Pujitha"
+Second_name = "Rama"
+Full_name = first_name +" "+Second_name
+print(Full_name)
+
+#Manipulating of Strings
+y = "Hello , World! "
+print(y.count('l'))
+print(y.lower())
+print(y.upper())
