519ee5fe17bba9ad4d0f32c7a16bae49b0fb7bf7
Pujitha <pujithamayakunta16@gmail.com>
2026-06-09 20:03:03 +0530

    Update employe.py

--- employe.py
+++ employe.py
@@ -1,3 +1,4 @@
+#THIS IS A SIMPLE CONDITIONAL PROGRAM PROGRAM USING LOGICAL AND OPERATOR
 experience = int(input("Enter years of experience:"))
 rating = int(input("Enter rating of employee:"))
 if experience >=5 and rating >=5:
