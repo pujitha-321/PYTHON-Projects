370dc138ba8d42dcd78076ff6d5e98e93434a290
Pujitha <pujithamayakunta16@gmail.com>
2026-06-09 20:05:30 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,8 @@
+name = "pujitha"
+username = input("Enter username:")
+id = 1234
+password = int(input("Enter password:"))
+if name== username and id==password:
+    print("proceed Login process")
+else:
+    print("Your login details are not matched")
