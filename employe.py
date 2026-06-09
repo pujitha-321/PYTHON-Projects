f03cb1608070762c7b9a977dbb46eeb372ff9cd7
Pujitha <pujithamayakunta16@gmail.com>
2026-06-09 20:02:02 +0530

    Add files via upload

---
+++ employe.py
@@ -0,0 +1,6 @@
+experience = int(input("Enter years of experience:"))
+rating = int(input("Enter rating of employee:"))
+if experience >=5 and rating >=5:
+    print("Employee eligible for bonus")
+else:
+    print("Employee not eligible for bonus")
