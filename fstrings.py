aeea6a28bc1bb44b4a6a7207c4b8c26286beea70
Pujitha <pujithamayakunta16@gmail.com>
2026-06-17 11:14:52 +0530

    Add files via upload

---
+++ fstrings.py
@@ -0,0 +1,11 @@
+name = "pujitha"
+age = 19
+print(f"your name is {name} and {age}years old!")
+
+#FORMAT SPECIFIERS
+price1 = 4.675
+price2 = -7.8975
+price3 = 23.78
+print(f"Price is {price1:.1f}")
+print(f"Price is {price2:.1f}")
+print(f"Price is {price3:.1f}")
