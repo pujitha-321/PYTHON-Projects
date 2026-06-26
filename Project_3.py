62e326094a2a696a95b2692771881f036cb191da
Pujitha <pujithamayakunta16@gmail.com>
2026-06-26 10:50:23 +0530

    Add files via upload

---
+++ Project_3.py
@@ -0,0 +1,34 @@
+Balance = 5000
+
+while True:
+    print("\n1.Check Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        print("Balance:", Balance)
+
+    elif choice == 2:
+        amount = int(input("Enter the deposited amount: "))
+        Balance += amount
+        print("The total Balance is:", Balance)
+
+    elif choice == 3:
+        withdraw_amount = int(input("Enter the withdraw amount: "))
+
+        if Balance >= withdraw_amount:
+            Balance -= withdraw_amount
+            print("Your Balance is:", Balance)
+            print("Amount withdrawn Successfully!")
+        else:
+            print("Insufficient Balance!")
+
+    elif choice == 4:
+        print("Exit the transaction")
+        break
+
+    else:
+        print("Invalid choice")
