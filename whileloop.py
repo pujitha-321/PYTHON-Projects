edb6d7a386be6ffba36a135a75b8ef00ad613220
Pujitha <pujithamayakunta16@gmail.com>
2026-07-07 21:27:25 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,13 @@
+
+name = ""
+while len(name) == 0:
+    name = input("Enter your name: ")
+print("hello"+name)
+
+#FOR LOOP
+for i in range(10):
+    print(i)
+
+for i in range(50,65+1):
+     print(i)
+     
