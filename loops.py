24cb24b45d6d10475c7c758e8a0437925553248e
Pujitha <pujithamayakunta16@gmail.com>
2026-07-09 20:11:48 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,28 @@
+num = 1
+while num <= 5:
+    print(num)
+    j = 1
+    while j<=3:
+        print(j)
+        j += 1
+    num += 1
+print()
+
+#For LOOP
+data = [3,'puji',4.6,'java']
+for value in data:
+    print(value)
+
+#CONTINUE STATEMENT
+for value in range(10):
+    if value == 5:
+        continue
+    print(value)
+print("Goodbye")
+
+#BREAK STATEMENT
+for value in range(10):
+    if value == 6:
+        break
+    print(value)
+print("byee bye")
